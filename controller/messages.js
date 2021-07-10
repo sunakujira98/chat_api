f78e72ac6990ad70e249ede98aace1e264847b49
Steven Theodore <steventheodore98@gmail.com>
2021-07-11 01:40:35 +0700

    Feat : 1. add get conversations with unread count

--- controller/messages.js
+++ controller/messages.js
@@ -73,30 +73,74 @@ exports.sendMessage = async (req, res) => {
 
 exports.getMessagesByConvId = (req, res) => {
   const { convId } = req.params
-  let sql = "SELECT * from messages WHERE conversation_id = ?";
-  let query = mysql.format(sql, convId)
+  const sql = "SELECT * from messages WHERE conversation_id = ?";
+  const query = mysql.format(sql, convId)
 
-  con.query(query, (err, recods) => {
+  con.query(query, (err, records) => {
     if (err) throw err
-    if (recods.length === 0) {
+    if (records.length === 0) {
       res.status(400).send({error: 'Data not available'})
     } else {
-      res.status(200).send(recods)
+      res.status(200).send(records)
     }
   })
 }
 
-exports.getConversationsById = (req, res) => {
-  const { id } = req.params
-  let sql = "SELECT c.*, u1.phone_number as phone_1, u2.phone_number as phone_2 from conversations c JOIN users u1 ON u1.id = c.user_1 JOIN users u2 ON u2.id = c.user_2 WHERE c.user_1 = ? OR c.user_2 = ?";
-  let query = mysql.format(sql, [id, id])
+exports.getConversationsByUserId = (req, res) => {
+  const { userId } = req.params
+  const sql = "SELECT c.*, u1.phone_number as phone_1, u2.phone_number as phone_2, MAX(m.send_date) as last_message_time from conversations c JOIN users u1 ON u1.id = c.user_1 JOIN users u2 ON u2.id = c.user_2 INNER JOIN messages m ON m.conversation_id = c.id WHERE c.user_1 = ? OR c.user_2 = ? GROUP BY c.id";
+  const query = mysql.format(sql, [userId, userId])
 
-  con.query(query, (err, recods) => {
+  con.query(query, (err, records) => {
     if (err) throw err
-    if (recods.length === 0) {
+    if (records.length === 0) {
       res.status(400).send({error: 'Data not available'})
     } else {
-      res.status(200).send(recods)
+      res.status(200).send(records)
+    }
+  })
+}
+
+
+exports.getConversationsWithUnreadCount = (req, res) => {
+  const { userId } = req.params
+  const sql = "SELECT c.*, u1.phone_number as phone_1, u2.phone_number as phone_2, MAX(m.send_date) as last_message_time FROM conversations c JOIN users u1 ON u1.id = c.user_1 JOIN users u2 ON u2.id = c.user_2 INNER JOIN messages m ON m.conversation_id = c.id WHERE (c.user_1 = ? OR c.user_2 = ?) GROUP BY c.id";
+
+  const query = mysql.format(sql, [userId, userId, userId])
+
+  con.query(query, (err, records) => {
+    if (err) throw err
+
+    if (records.length === 0) {
+      res.status(400).send({error: 'Data not available'})
+    } else {
+      // get conversation id from the records
+      const conversationId = records.map(row => row.id)
+
+      const sql = `SELECT * FROM messages WHERE conversation_id IN (?) AND is_read = false`
+      const query = mysql.format(sql, [conversationId])
+
+      con.query(query, (err, rows) => {
+        if (err) throw err
+
+        for (let i = 0; i < records.length; i++) {
+          let unreadCount = 0
+          for (let j in rows) {
+            if (records[i].id === rows[j].conversation_id) {
+              if (userId != rows[j].sender_user_id) {
+                unreadCount++
+              }
+            }
+          }
+          records[i].unread_count = unreadCount
+        }
+
+        if (rows.length === 0) {
+          res.status(400).send({error: 'Data not available'})
+        } else {
+          res.status(200).send(records)
+        }
+      })
     }
   })
 }
--- router.js
+++ router.js
@@ -4,8 +4,9 @@ const router = express.Router({ mergeParams: true })
 router.get('/users', require('./controller/users').getUsers)
 
 router.post('/message', require('./controller/messages').sendMessage)
-router.get('/message/:convId', require('./controller/messages').getMessagesByConvId)
+router.get('/message/conversation/:convId', require('./controller/messages').getMessagesByConvId)
 
-router.get('/conversation/:id', require('./controller/messages').getConversationsById)
+router.get('/conversation/user/:userId', require('./controller/messages').getConversationsByUserId)
+router.get('/conversation/user-unread/:userId', require('./controller/messages').getConversationsWithUnreadCount)
 
 module.exports = router
